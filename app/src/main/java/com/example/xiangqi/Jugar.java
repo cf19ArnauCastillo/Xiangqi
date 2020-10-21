9ff9ece3457a7ab2cf38227ab686583e94d24ae2
cf19arnau.castillo <cf19arnau.castillo@iesjoandaustria.org>
2020-10-21 17:12:44 +0200

    En proceso el Jugar.java

--- app/src/main/java/com/example/xiangqi/Jugar.java
+++ app/src/main/java/com/example/xiangqi/Jugar.java
@@ -2,10 +2,14 @@ package com.example.xiangqi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * @Arnau_Castillo
@@ -16,6 +20,49 @@ public class Jugar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jugar);
-    }
 
+        String nombre = "";
+        String nombre2 = "";
+        String color1 = "";
+        String color2 = "";
+
+        final EditText jugador1 = findViewById(R.id.nplayer1);
+        final EditText jugador2 = findViewById(R.id.nplayer2);
+
+        final Button guardar1 = findViewById(R.id.infoplayer1);
+        final Button guardar2 = findViewById(R.id.infoplayer2);
+        guardar1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                String nplayer1 = jugador1.getText().toString();
+                String nplayer2 = jugador2.getText().toString();
+
+                if (nplayer1!=("")) {
+                    //LOGIN OK
+                    //Aqui podemos hacer que salga un mensajito un momento abajo    resultat.setText("Login OK");
+                    goToTablero();
+                }else{
+                    //LOGIN NO OK
+                    //ESTO EN EL FUTURO BEIBE  resultat.setText("Login OKN'T");
+                }
+            }
+        });
+        guardar2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                String nplayer2 = jugador2.getText().toString();
+
+                if (nplayer2!=("")) {
+                    //LOGIN OK
+                    //Aqui podemos hacer que salga un mensajito un momento abajo    resultat.setText("Login OK");
+                    goToTablero();
+                }else{
+                    //LOGIN NO OK
+                    //ESTO EN EL FUTURO BEIBE  resultat.setText("Login OKN'T");
+                }
+            }
+        });
+    }
+    public void goToTablero(){
+        Intent IntentMenu = new Intent(this,Tablero.class);
+        startActivity(IntentMenu);
+    }
 }
---
+++ app/src/main/java/com/example/xiangqi/Tablero.java
@@ -0,0 +1,14 @@
+package com.example.xiangqi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tablero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tablero);
+    }
+}
