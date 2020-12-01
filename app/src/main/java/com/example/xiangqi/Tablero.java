fc32cd8e10de1f9d4f5643d34d954dac65fbe691
cf19arnau.castillo <cf19arnau.castillo@iesjoandaustria.org>
2020-12-01 16:31:29 +0100

    (tablero)

--- app/src/main/java/com/example/xiangqi/Tablero.java
+++ app/src/main/java/com/example/xiangqi/Tablero.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
