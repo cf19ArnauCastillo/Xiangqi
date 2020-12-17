ee4684de531ae28370e3a39a369796b7f501d928
cf19arnau.castillo <cf19arnau.castillo@iesjoandaustria.org>
2020-12-17 17:25:52 +0100

    workin hard

--- app/src/main/java/com/example/xiangqi/Piezas/Rey.java
+++ app/src/main/java/com/example/xiangqi/Piezas/Rey.java
@@ -14,18 +14,22 @@ public class Rey extends  Pieza {
     public void printGhost(int row, int col, Pieza[][]tablero, int color){
 //        Log.i("infoPieza", Integer.toString(row) + " -" + Integer.toString(col));
 
-        createGhost(row+1,col+1,color,tablero);
-        createGhost(row+1,col-1,color,tablero);
-        createGhost(row-1,col+1,color,tablero);
-        createGhost(row-1,col-1,color,tablero);
         createGhost(row+0,col+1,color,tablero);
         createGhost(row+0,col-1,color,tablero);
         createGhost(row+1,col+0,color,tablero);
         createGhost(row-1,col-0,color,tablero);
-
     }
     public void createGhost(int row,int col,int color,Pieza[][] tablero){
-        if(row>=0 && row<=9 && col >=0 && col <=9){
+        if(color==1 && row>=0 && row<=2 && col >=3 && col <=5){
+            if(tablero[row][col]==null){
+                tablero[row][col]=new Ghost(color);
+            }else{
+                if(tablero[row][col].color!=this.color){
+                    tablero[row][col].isEatable=true;
+                }
+            }
+        }
+        if(color==0 && row>=7 && row<=9 && col >=3 && col <=5){
             if(tablero[row][col]==null){
                 tablero[row][col]=new Ghost(color);
             }else{
--- app/src/main/java/com/example/xiangqi/Piezas/Vasallo.java
+++ app/src/main/java/com/example/xiangqi/Piezas/Vasallo.java
@@ -18,14 +18,18 @@ public class Vasallo extends Pieza {
         createGhost(row+1,col-1,color,tablero);
         createGhost(row-1,col+1,color,tablero);
         createGhost(row-1,col-1,color,tablero);
-        createGhost(row+0,col+1,color,tablero);
-        createGhost(row+0,col-1,color,tablero);
-        createGhost(row+1,col+0,color,tablero);
-        createGhost(row-1,col-0,color,tablero);
-
     }
     public void createGhost(int row,int col,int color,Pieza[][] tablero){
-        if(row>=0 && row<=9 && col >=0 && col <=9){
+        if(color==1 && row>=0 && row<=2 && col >=3 && col <=5){
+            if(tablero[row][col]==null){
+                tablero[row][col]=new Ghost(color);
+            }else{
+                if(tablero[row][col].color!=this.color){
+                    tablero[row][col].isEatable=true;
+                }
+            }
+        }
+        if(color==0 && row>=7 && row<=9 && col >=3 && col <=5){
             if(tablero[row][col]==null){
                 tablero[row][col]=new Ghost(color);
             }else{
